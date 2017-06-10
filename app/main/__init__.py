95df0655a70d6266f6d42cdf81f820046b11141c
wills <18240440420@163.com>
2017-06-10 20:37:25 +0800

    第一次整合

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 # 创建蓝本以定义路由
 # 程序的路由保存在包里的app/main/views.py 模块中，而错误处理程序保存在app/main/
 # errors.py 模块中。导入这两个模块就能把路由和错误处理程序与蓝本关联起来
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__, static_url_path='', static_folder='static')
 from . import views, errors
